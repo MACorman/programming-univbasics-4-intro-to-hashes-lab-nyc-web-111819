c1a5c463e24f92c2de1fd83a0c1de47c8faa433b
Madeline Corman <madelinecorman@gmail.com>
2019-11-10 01:20:40 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,45 +1,29 @@
-require 'pry'
 def new_hash
-  new_hash = {}
+  {}
 end
 
 def my_hash
-  my_hash = {
-    name: "Madeline Corman",
-    age: 24
-  }
+  {name: "Madeline Corman",
+    age: 24}
 end
 
 def pioneer
- pioneer = {
-   name: "Grace Hopper"
- }
+ {name: "Grace Hopper"}
 end
 
 def id_generator
-  id_generator = {
-    id: 43
-  }
-  binding.pry
+  {id: 43}
 end
 
 def my_hash_creator(key, value)
-my_hash_creator = {}
-my_hash_creator[key] = value
-return my_hash_creator
+{key => value}
 end
 
 def read_from_hash(hash, key)
-  
-  #read_from_hash = {}
-  #hash = {} 
-  #read_from_hash[hash] = key
-
-
+  hash[key]
 end
 
 def update_counting_hash(hash, key)
-  # given a hash an a key as parameters, return an updated hash
-  # if the provided key is not present in the hash, add it and assign it to the value of 1
-  # if the provided key is present, increment its value by 1
+  hash[key] ? hash[key]+=1 : hash[key] = 1
+  hash 
 end
