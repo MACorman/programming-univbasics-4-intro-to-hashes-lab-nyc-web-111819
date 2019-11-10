a860e649b876f22b8267e512155fb9e07c498392
Madeline Corman <madelinecorman@gmail.com>
2019-11-10 01:12:04 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -16,16 +16,11 @@ def id_generator
 end
 
 def my_hash_creator(key, value)
-{key
+{key => value}
 end
 
 def read_from_hash(hash, key)
-  
-  #read_from_hash = {}
-  #hash = {} 
-  #read_from_hash[hash] = key
-
-
+  {hash => key}
 end
 
 def update_counting_hash(hash, key)
